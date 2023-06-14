8fcf1f470b979d8740fca30d7b1e400efeedbf0b
Wouter Gerrits <wouter@cuneocashflow.com>
2023-06-14 18:49:57 +0200

    git ignore

---
+++ dist/components/inputs/textInput/components/TextInput.d.ts
@@ -0,0 +1,11 @@
+type Props = {
+    placeholder?: string;
+    backgroundColor?: string;
+    textColor?: string;
+    sendButtonColor?: string;
+    defaultValue?: string;
+    onSubmit: (value: string) => void;
+};
+export declare const TextInput: (props: Props) => import("solid-js").JSX.Element;
+export {};
+//# sourceMappingURL=TextInput.d.ts.map
